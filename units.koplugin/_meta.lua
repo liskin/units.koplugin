03d7f12f9a1f24bf7029b487a798492f926c1cb7
Tomas Janousek <tomi@nomi.cz>
2026-09-16 17:55:24 +0100

    ci: Add GitHub Actions and dependabot
    Copypaste from https://github.com/ckilb/pocketbooksync.koplugin

--- units.koplugin/_meta.lua
+++ units.koplugin/_meta.lua
@@ -2,4 +2,5 @@ local _ = require("gettext")
 return {
     fullname = _("Unit conversion (highlight action)"),
     description = _([[This plugin offers to convert units in highlighted (selected) text.]]),
+    version = "0.0.0", -- x-release-please-version
 }
